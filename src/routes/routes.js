cb461ac3dc67bd096fec2f57aa5addd601da8eec
James Junior <jamescosjr@gmail.com>
2024-10-08 05:01:56 -0300

    feat: add new routes for listing employees by salary and deleting/updating employees

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,9 @@ import {
   findEmployeeByNameHandler,
   listEmployeesByDepartmentHandler,
   listEmployeesByPositionHandler,
+  listEmployeesBySalaryHandler,
+  deleteEmployeeHandler,
+  updateEmployeeHandler,
 } from "../controllers/employeeController.js";
 
 const app = express();
@@ -15,5 +18,8 @@ app.get("/employees", listEmployeesHandler);
 app.get("/employees/:name", findEmployeeByNameHandler);
 app.get("/employees/department/:department", listEmployeesByDepartmentHandler);
 app.get("/employees/position/:position", listEmployeesByPositionHandler);
+app.get("/employees/salary/:salary", listEmployeesBySalaryHandler);
+app.delete("/employees/:id", deleteEmployeeHandler);
+app.put("/employees/:id", updateEmployeeHandler);
 
 export default app;
