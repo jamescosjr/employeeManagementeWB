b4462c1d9cefab7abed4e5c083ebb6a6b94cd399
James Junior <jamescosjr@gmail.com>
2024-10-09 11:14:40 -0300

    refactor: update import path for routes.js in server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-import app from "./src/routes/routes.js";
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const app = express();
+app.use(express.json());
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
